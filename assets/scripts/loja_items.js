06c489a250dc6de6fd009b3554f5b244fe916564
Mateus da Silveira <93267548+MateusSilver@users.noreply.github.com>
2023-02-26 21:18:26 -0300

    tentando carrinho em javascript

--- assets/scripts/loja_items.js
+++ assets/scripts/loja_items.js
@@ -1,33 +1,28 @@
-const itens = [
-    {
+const itens = [{
         id: 0,
         nome: '1618',
         autor: 'Cesar Bravo',
         preco: 70.00,
         quantidade: 0
-    },
-    {
+    },{
         id: 1,
         nome: 'Lady Killers',
         autor: 'Tori Telfer',
         preco: 58.00,
         quantidade: 0
-    },
-    {
+    },{
         id: 2,
         nome: 'PSICOSE',
         autor: 'Robert Bloch',
         preco: 48.00,
         quantidade: 0
-    },
-    {
+    },{
         id: 3,
         nome: 'Suicidas',
         autor: 'Raphael Montes',
         preco: 35.00,
         quantidade: 0
-    },
-    {
+    },{
         id: 4,
         nome: 'O Colecionador',
         autor: 'John Fowles',
@@ -46,9 +41,36 @@ inicializarLoja = () => {
         <h2>` + val.nome + `</h2>
         <p>`+ val.autor +`</p>
         <div class="preco">R$ ` + val.preco.toFixed(2).replace('.',',') + `</div>
-        <a key="` + val.id + `" href="">Adicionar ao carrinho</a>
+        <a id="add" key="` + val.id + `" href="">Adicionar ao carrinho</a>
         </div>`;
     });
 }
 
 inicializarLoja();
+
+atualizarCarrinho = () => {
+    console.log(itens);
+}
+
+var links = document.getElementById('add');
+
+for(var i = 0; i < links.length; i++){
+    links[i].addEventListener("click", function(){
+        let key = this.getAttribute('key');
+        itens[key].quantidade++;
+        atualizarCarrinho();
+        return false;
+    })
+}
+
+
+inicializarCarrinho = () => {
+    var carrinhos = document.getElementById('carrinho')
+    itens.map((val) => {
+        if(val.quantidade>0){
+            carrinhos.innerHTML += `
+            <p>` + val.nome + ` | quantidade: ` + val.quantidade + `</p>
+            <hr></hr>`
+        }
+    });
+}
